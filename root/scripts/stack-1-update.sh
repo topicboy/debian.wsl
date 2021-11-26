b3c49c83af94aee7033901906b048d84a585c9eb
root <root@BLACKBOX.localdomain>
2021-11-26 20:35:03 +0100

    backup.sh 2021-11-26 20:35:03

--- root/scripts/stack-1-update.sh
+++ root/scripts/stack-1-update.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cd /docker/stack-1
-docker-compose down
-docker-compose pull radarr sonarr readarr sabnzbd jackett calibre resilio-sync flaresolverr
-docker-compose up -d
+docker-compose down &&
+	docker-compose pull radarr sonarr readarr sabnzbd jackett calibre resilio-sync flaresolverr &&
+	docker-compose up -d
+
+
 echo "done!"
 exit 0
