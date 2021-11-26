da2129b6d1ccb3c9f5664086504b62cc6ff85b05
root <root@BLACKBOX.localdomain>
2021-11-26 20:36:03 +0100

    backup.sh 2021-11-26 20:36:03

--- root/scripts/stack-1-update.sh
+++ root/scripts/stack-1-update.sh
@@ -3,7 +3,8 @@ cd /docker/stack-1
 docker-compose down &&
 	docker-compose pull radarr sonarr readarr sabnzbd jackett calibre resilio-sync flaresolverr &&
 	docker-compose up -d
-
-
+if [ "$1" = "cron" ]; then
+        echo "crontab $(date '+%F %T')" >> /root/scripts/stack-1-update.log
+fi
 echo "done!"
 exit 0
