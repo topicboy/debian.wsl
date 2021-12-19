a5bddeca7295cf0d5dff40d26b45b48a4822ab1e
root <root@BLACKBOX.localdomain>
2021-12-19 20:00:03 +0100

    backup.sh 2021-12-19 20:00:03

--- root/scripts/stack-1-update.sh
+++ root/scripts/stack-1-update.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 cd /docker/stack-1
 docker-compose down &&
-docker-compose pull radarr sonarr readarr sabnzbd jackett calibre resilio-sync flaresolverr &&
-docker-compose up -d
+docker-compose pull radarr sonarrime sonarr readarr lazylib sabnzbd jackett calibre resilio flaresolverr &&
+docker-compose up -d &&
+docker image prune -f &&
+docker volume prune -f
+
 if [ "$1" = "cron" ]; then
         echo "crontab $(date '+%F %T')" >> /root/scripts/stack-1-update.log
 fi
+
 echo "done!"
 exit 0
